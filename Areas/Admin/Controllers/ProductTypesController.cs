cc9832c6d813334ef1bdb5f3aea05cdb0cdec26d
Emon <emon.onnorokom@gmail.com>
2025-07-21 18:06:52 +0600

    Added the insertion function for Product Types

---
+++ Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,67 @@
+﻿using Bangaliyana.Data;
+using Bangaliyana.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bangaliyana.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var data = _db.ProductTypes.ToList();
+            return View(data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(productTypes);
+        }
+       
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(productTypes);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Bangaliyana.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bangaliyana.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
